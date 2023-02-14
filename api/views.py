96cb1dfd2cc4e140ddd97ad10d4175e1f4ed199d
Razi Ahmad <mail2razi4u@gmail.com>
2023-02-14 14:54:16 +0530

    views created

--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from datetime import datetime
+from .models import Reminders
+from .serializer import RemindersSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class reminderAPI(APIView):
+    def get(self,request,pk=None,format=None):
+        reminders = Reminders.objects.all()
+        serializer = RemindersSerializer(reminders,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def post(self,request,pk=None,format=None):
+        dt = request.data.get("dataTime")
+        cur = datetime.now()
+        if(dt<=cur):
+            res = {"msg":"Invalid datetime, As you date time has passed already"}
+            return Response(res,status=status.HTTP_400_BAD_REQUEST)
+        deserializer = RemindersSerializer(data=request.data)
+        if(deserializer.is_valid()):
+            deserializer.save()
+            res = {"msg":"Reminder Created Successfully"}
+            return Response(res,status=status.HTTP_201_CREATED)
+        return Response(deserializer.errors,status=status.HTTP_400_BAD_REQUEST)
