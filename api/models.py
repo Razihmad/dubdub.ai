faa3b74b455da3a707692abf98d8e3f8764370ae
Razi Ahmad <mail2razi4u@gmail.com>
2023-02-14 14:40:57 +0530

    models created and registered

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Reminders(models.Model):
+    work  = models.CharField(max_length=10000)
+    dateTime = models.DateTimeField()
+    status = models.CharField(max_length=100,choices=(("Incomplete","Incomplete"),("Complete","Complete")))
+    
