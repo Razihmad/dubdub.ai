4dc4a2eb423fd7f9ddfecaf86c55c4a33085256f
Razi Ahmad <mail2razi4u@gmail.com>
2023-02-14 15:37:49 +0530

    error handling

--- api/views.py
+++ api/views.py
@@ -11,12 +11,13 @@ class reminderAPI(APIView):
         reminders = Reminders.objects.all()
         serializer = RemindersSerializer(reminders,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
     def post(self,request,pk=None,format=None):
         dt = request.data.get("dateTime")
         cur = datetime.now()
         d = datetime.strptime(dt, "%d-%b-%Y-%H:%M:%S")
         if(d<=cur):
-            res = {"msg":"Invalid datetime, As you date time has passed already"}
+            res = {"msg":"Invalid datetime, As your date time has passed already"}
             return Response(res,status=status.HTTP_400_BAD_REQUEST)
         deserializer = RemindersSerializer(data=request.data)
         if(deserializer.is_valid()):
@@ -24,3 +25,28 @@ class reminderAPI(APIView):
             res = {"msg":"Reminder Created Successfully"}
             return Response(res,status=status.HTTP_201_CREATED)
         return Response(deserializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self,request,pk=None,format=None):
+        id = request.data.get('id')
+        if(Reminders.objects.filter(pk=id).exists()):
+            reminder = Reminders.objects.get(pk=id)
+            deserializer = RemindersSerializer(reminder,data=request.data,partial=True)
+            if(deserializer.is_valid()):
+                deserializer.save()
+                res = {"msg":"Updated Successfully"}
+                return Response(res,status=status.HTTP_200_OK)
+            return Response(deserializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        res = {"msg":"No Such Id"}
+        return Response(res,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,pk=None,format=None):
+        id = request.data.get('id')
+        try:
+            rem = Reminders.objects.get(pk=id)
+            rem.delete()
+            res = {"msg":"Deleted Successfully"}
+            return Response(res,status=status.HTTP_200_OK)
+        except:
+            res = {"msg":"Id does not exist"}
+            return Response(res,status=status.HTTP_400_BAD_REQUEST)
+        
